cf3c29704d2e4832a87ee7684b079faeec2ced6b
abmptrm <adilbimoadil123@gmail.com>
2024-12-27 20:22:28 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NavbarComponent from "./components/component.navbar";
+import { Button } from "flowbite-react";
+
+function App() {
+
+  return (
+    <>
+      <NavbarComponent />
+      <Button>Click me</Button>
+    </>
+
+  );
+}
+
+export default App;
