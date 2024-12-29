61c76f186ad5c1fc47c74c8dacffabffdea442a2
abmptrm <adilbimoadil123@gmail.com>
2024-12-29 21:18:11 +0700

    tes

--- src/components/component.navbar.jsx
+++ src/components/component.navbar.jsx
@@ -1,23 +1,45 @@
+import { useState } from "react";
 import { Button, Navbar } from "flowbite-react";
-import Logo from '../assets/react.svg';
+import Logo from '../assets/leaf.png';
 
 function NavbarComponent() {
+
+    const [activeLink, setActiveLink] = useState("Home");
+
+    const handleSetActive = (name) => {
+        setActiveLink(name);
+    };
+
     return (
-        <Navbar fluid rounded>
-            <Navbar.Brand href="https://flowbite-react.com">
-                <img src={Logo} className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+        <Navbar fluid rounded className="bg-satu z-10">
+            <Navbar.Brand href="https://flowbite-react.com" className="px-2 py-3">
+                <img src={Logo} className="mr-3 h-6 sm:h-9" alt="Aeroponics" />
+                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Aeroponics</span>
             </Navbar.Brand>
             <div className="flex md:order-2">
-                <Button>Get started</Button>
+                <Button className="hidden md:block bg-lima">Get started</Button>
                 <Navbar.Toggle />
             </div>
             <Navbar.Collapse>
-                <Navbar.Link href="#" active>Home</Navbar.Link>
-                <Navbar.Link href="#">About</Navbar.Link>
-                <Navbar.Link href="#">Services</Navbar.Link>
-                <Navbar.Link href="#">Pricing</Navbar.Link>
-                <Navbar.Link href="#">Contact</Navbar.Link>
+                {["Home", "About", "Services", "Pricing", "Contact", "Project"].map((link) => (
+                    <Navbar.Link
+                        key={link}
+                        href="#"
+                        onClick={() => handleSetActive(link)}
+                        style={{ 
+                            .active:hover {
+                                
+                            }
+                        }}
+                        className={`rounded-md !hover:text-lima !md:hover:text-lima text-md md:text-[1.2em] text-center my-[0.2rem] ${activeLink === link
+                            ? "text-lima font-semibold"
+                            : "!hover:text-lima"
+                            }`}
+                    >
+                        {link}
+                    </Navbar.Link>
+                ))}
+                <Button className="my-3 block md:hidden bg-lima">Get started</Button>
             </Navbar.Collapse>
         </Navbar>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,22 @@
 const flowbite = require("flowbite-react/tailwind");
 
 export default {
+  important: true,
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     flowbite.content(),
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        satu : "#F0EAD2",
+        dua: "#DDE5B6",
+        tiga: "#ADC178",
+        empat: "#A98467",
+        lima : "#6C584C",
+      }
+    },
   },
   plugins: [
     flowbite.plugin(),
